e56c19594decd524117a519f4af402c7c7db60e7
VadimIshkarin <theishkarin@gmail.com>
2022-10-19 15:47:38 -0400

    Added changes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-
 import { Form, Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import "./login-view.scss";
